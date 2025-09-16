188bd5480dfb8f9c7c10583e2d573c7bf71d3c41
Saba Fakhri <sabafakhri71@gmail.com>
2025-09-16 10:44:40 +0300

    fix

---
+++ components/ui/SortBy.tsx
@@ -0,0 +1,33 @@
+type SortByProps = {
+  value: "Instructor" | "Price";
+  onChange: (val: "Instructor" | "Price") => void;
+};
+
+import * as React from "react";
+
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+export default function SortBy({ value, onChange }: SortByProps) {
+  return (
+    <Select onValueChange={(val) => onChange(val as "Instructor" | "Price")}>
+      <SelectTrigger className="w-full">
+        <SelectValue placeholder="Sort by" />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectGroup>
+          <SelectLabel></SelectLabel>
+          <SelectItem value="Instructor">Instructor</SelectItem>
+          <SelectItem value="Price">Price</SelectItem>
+        </SelectGroup>
+      </SelectContent>
+    </Select>
+  );
+}
