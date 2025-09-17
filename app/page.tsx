61be8d56ef7ec3566ac43a39d5c7018167bb90c0
Saba Fakhri <sabafakhri71@gmail.com>
2025-09-17 11:54:54 +0300

    fix: simplify JSX syntax for OtherCourses component

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 sm:p-20">
-     <OtherCourses></OtherCourses>
+     <OtherCourses/>
      <Subscribe/>
     </div>
   );
