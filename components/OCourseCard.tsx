bccdacee44a3390c2b52b7a3c80e6a3da7aed04a
Saba Fakhri <sabafakhri71@gmail.com>
2025-09-26 19:14:58 +0300

    fix:Course Card styles

--- components/OCourseCard.tsx
+++ components/OCourseCard.tsx
@@ -1,29 +1,36 @@
 import type { Course } from "@/types/types";
 import { Card, CardAction, CardContent, CardTitle } from "./ui/card";
 import { Button } from "./ui/button";
+import Image from "next/image";
+import { ShoppingBagIcon } from "./icons/Icons";
 
 export default function OCourseCard({ course }: { course: Course }) {
   return (
-    <Card key={course.id} className="">
-      <CardContent className="flex items-center">
-        <img
+    <Card key={course.id} className="relative">
+      <CardContent className="flex items-center gap-[15px]">
+        <Image
           src={course.imageUrl}
           alt={course.title}
-          className="h-26 w-40 object-cover"
+          width={76}
+          height={87}
         />
         <div className="flex flex-col gap-2">
           <CardTitle>{course.title}</CardTitle>
-          <p className="">
+          <p className="text-orange-200">
             {Array(5)
               .fill(0)
               .map((_, i) => (
                 <span key={i}>{i < course.rating ? "★" : "☆"}</span>
               ))}
           </p>
-          <div>{course.price}</div>
+          <div className="text-16-20 font-semibold text-orange-900">
+            {"$" + course.price}
+          </div>
         </div>
-        <CardAction className="relative">
-          <Button variant="default"></Button>
+        <CardAction className="absolute right-[15px] bottom-[15px]">
+          <Button variant="ShoppingBag" size="ShoppingBag">
+            <ShoppingBagIcon />
+          </Button>
         </CardAction>
       </CardContent>
     </Card>
