f6adba7edade8fcb30ffcf16e99af77ea44e2cfa
Saba Fakhri <sabafakhri71@gmail.com>
2025-09-26 19:16:45 +0300

    fix: shoppingBag var

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -31,6 +31,7 @@ const buttonVariants = cva(
           "border border-purple-900 !text-purple-900 font-medium text-16-20 text-center hover:bg-purple-900 hover:!text-background",
         Lessons:
           "text-14-20 bg-purple-900 rounded-[5px] !text-background !dark:text-foreground hover:bg-purple-900/90",
+        ShoppingBag: "bg-purple-100 rounded-[5px] dark:bg-purple-200 ",
         JoinCourses:
           "p-3.75 text-14-30 text-purple-900 lg:text-16-20 bg-purple-300 rounded-[5px] dark:text-foreground lg:rounded-[10px] transition-all duration-300 hover:bg-purple-300/90 font-normal",
       },
@@ -47,6 +48,7 @@ const buttonVariants = cva(
         Lessons: "h-[50px] w-43.5",
         JoinCourses: "h-11",
         HomeHedear: "h-12.5",
+        ShoppingBag: "size-8.5 sm:size-11",
       },
     },
     defaultVariants: {
